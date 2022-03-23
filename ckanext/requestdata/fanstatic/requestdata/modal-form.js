f67a27da53734666cdc8eacc78ee73f1e38ac3ee
Dan Mihaila <danmihaila@gmail.com>
2022-03-23 14:55:13 +0200

    HDX-8133 requestdata py3

--- ckanext/requestdata/fanstatic/requestdata/modal-form.js
+++ ckanext/requestdata/fanstatic/requestdata/modal-form.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 /* modal-form
  *
@@ -30,11 +30,11 @@ this.ckan.module('modal-form', function($) {
         // Whether or not the rendered snippet has already been received from CKAN.
         _snippetReceived: false,
         _onClick: function(event) {
-            var is_current_user_a_maintainer = this.options.is_current_user_a_maintainer
-            var dialogResult = true
+            var is_current_user_a_maintainer = this.options.is_current_user_a_maintainer;
+            var dialogResult = true;
 
             if (is_current_user_a_maintainer === 'True') {
-                var dialogResult = window.confirm('Request own dataset\n\nWARNING: You are a maintainer of the dataset you are requesting. Do you wish to continue making this request?')
+                var dialogResult = window.confirm('Request own dataset\n\nWARNING: You are a maintainer of the dataset you are requesting. Do you wish to continue making this request?');
             }
 
             if (dialogResult) {
@@ -55,7 +55,7 @@ this.ckan.module('modal-form', function($) {
                     maintainers: JSON.stringify(this.options.post_data.maintainers),
                     requested_by: this.options.post_data.requested_by,
                     sender_id: this.options.post_data.sender_id
-                }
+                };
                 if (!this._snippetReceived) {
                     this.sandbox.client.getTemplate(this.options.template_file, payload, this._onReceiveSnippet);
                     this._snippetReceived = true;
@@ -88,7 +88,7 @@ this.ckan.module('modal-form', function($) {
                 element.modal({
                     show: false
                 });
-                this.modalFormError = this.modal.find('.alert-error')
+                this.modalFormError = this.modal.find('.alert-error');
             }
             return this.modal;
         },
@@ -96,46 +96,46 @@ this.ckan.module('modal-form', function($) {
             var base_url = ckan.sandbox().client.endpoint;
             var url = base_url + this.options.submit_action || '';
             var data = this.options.post_data || '';
-            var form = this.modal.find('form')
-            var formElements = $(form[0].elements)
-            var submit = true
+            var form = this.modal.find('form');
+            var formElements = $(form[0].elements);
+            var submit = true;
             var formData = new FormData();
 
             // Clear form errors before submitting the form.
-            this._clearFormErrors(form)
+            this._clearFormErrors(form);
 
             for (var item in data) {
-              formData.append(item, data[item])
+              formData.append(item, data[item]);
             }
 
             // Add field data to payload data
             $.each(formElements, function(i, element) {
-                var value = element.value.trim()
+                var value = element.value.trim();
 
                 if (element.required && value === '') {
-                    var hasError = element.parentElement.querySelector('.error-block')
+                    var hasError = element.parentElement.querySelector('.error-block');
 
                     if (!hasError) {
-                        this._showInputError(element, 'Missing value')
+                        this._showInputError(element, 'Missing value');
                     }
 
-                    submit = false
+                    submit = false;
                 } else {
-                    if (element.type == 'file') {
+                    if (element.type === 'file') {
                        if (element.files.length > 0) {
-                             formData.append(element.name, element.files[0], element.value)
+                             formData.append(element.name, element.files[0], element.value);
 
                              // If a file has been attached, than move the request to archive
                             // and mark it that data has been shared
 
-                             formData.append('state', 'archive')
-                             formData.append('data_shared', true)
+                             formData.append('state', 'archive');
+                             formData.append('data_shared', true);
                       }
                     } else {
-                      formData.append(element.name, element.value)
+                      formData.append(element.name, element.value);
                     }
                 }
-            }.bind(this))
+            }.bind(this));
 
             if (submit) {
               $.ajax({
@@ -146,7 +146,7 @@ this.ckan.module('modal-form', function($) {
                 type: 'POST'
               })
                 .done(function(data) {
-                    data = JSON.parse(data)
+                    data = JSON.parse(data);
                     if (data.error && data.error.fields) {
                         for(var key in data.error.fields){
                             this._showFormError(data.error.fields[key]);
@@ -163,14 +163,14 @@ this.ckan.module('modal-form', function($) {
                         }
                     }
                 }.bind(this))
-                .error(function(error) {
-                    this._showFormError(error.statusText)
-                }.bind(this))
+                .fail(function(error) {
+                    this._showFormError(error.statusText);
+                }.bind(this));
             }
         },
         _onCancel: function(event) {
             this._snippetReceived = false;
-            this._clearFormErrors()
+            this._clearFormErrors();
             this._resetModalForm();
         },
         _showInputError: function(element, message) {
@@ -181,32 +181,32 @@ this.ckan.module('modal-form', function($) {
             element.parentElement.appendChild(div);
         },
         _clearFormErrors: function() {
-            var errors = this.modal.find('.error-block')
+            var errors = this.modal.find('.error-block');
 
             $.each(errors, function(i, error) {
-                error.parentElement.removeChild(error)
-            })
+                error.parentElement.removeChild(error);
+            });
 
-            this.modalFormError.addClass('hide')
-            this.modalFormError.text('')
+            this.modalFormError.addClass('hide');
+            this.modalFormError.text('');
         },
         _showFormError: function(message) {
-            this.modalFormError.removeClass('hide')
-            this.modalFormError.text(message)
+            this.modalFormError.removeClass('hide');
+            this.modalFormError.text(message);
         },
         _showSuccessMsg: function(msg) {
             var div = document.createElement('div');
             div.className = "alert alert-success alert-dismissable fade in";
-            div.id = 'request-success-container'
+            div.id = 'request-success-container';
             div.textContent = msg;
             div.style.marginTop = '25px';
-            var currentDiv = $('.requested-data-message')
+            var currentDiv = $('.requested-data-message');
 
             if (currentDiv.length > 1) {
                 currentDiv = this.el.next('.requested-data-message');
             }
             currentDiv.css('display', 'block');
-            currentDiv.append(div)
+            currentDiv.append(div);
             this._resetModalForm();
         },
         _resetModalForm: function(){
